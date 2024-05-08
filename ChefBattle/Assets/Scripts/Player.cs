9c9105a886b797ed9ab7f4bd8a961c81a00379dc
YSP <65054425+carmendetroit@users.noreply.github.com>
2024-05-08 23:59:16 +0300

    Character Change
    Since at the first iteration the character adjustment will be hard, I decided to change to a simpler character.

--- ChefBattle/Assets/Scripts/Player.cs
+++ ChefBattle/Assets/Scripts/Player.cs
@@ -4,19 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 2.0f;
-    private float moveSpeed_dyn;
-    private bool isWalking;
-    private bool isDancing = false;
-
-    [SerializeField] private float runSpeed = 3.0f;
+    [SerializeField] private float speed = 7f;
     private void Update()
     {
-        moveSpeed_dyn = moveSpeed; 
         Vector2 inputVector = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.W))
         {
-            inputVector.y = +1;
+            inputVector.y = 1;
         }
         if (Input.GetKey(KeyCode.S))
         {
@@ -28,36 +22,13 @@ public class Player : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.D))
         {
-            inputVector.x = +1;
-        }
-        if (Input.GetKey(KeyCode.LeftShift)){
-            moveSpeed_dyn = runSpeed;
-        }
-        if (Input.GetKey(KeyCode.X))
-        {
-            isDancing= true;
-        }else{
-            isDancing=false;
+            inputVector.x = 1;
         }
         inputVector = inputVector.normalized;
-        Vector3 movedir = new Vector3(inputVector.x, 0f, inputVector.y);    
-        float playersize = 0.7f;
-        bool canmove = !Physics.Raycast(transform.position, movedir, playersize);
-        if (canmove)
-        {
-            transform.position += movedir * Time.deltaTime * moveSpeed_dyn;
-
-        }
-        isWalking = movedir != Vector3.zero;
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, movedir, Time.deltaTime * rotateSpeed);
-    }
-    public bool IsWalking()
-    {
-        return isWalking;
-    }
-    public bool IsDancing()
-    {
-        return isDancing;
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDirection * speed * Time.deltaTime;
+        float rotationSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward,moveDirection,Time.deltaTime *rotationSpeed);
+        Debug.Log(inputVector);
     }
 }
