51b259d97d98bc88a7d22a91e1020a6d651f3ff5
Furkan Bülbül <furknblbl@gmail.com>
2024-05-20 21:26:40 +0300

    remove redundant import

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Security.Cryptography;
+
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager: Singleton<GameManager>
